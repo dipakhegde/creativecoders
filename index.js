08df941fb8fe504383a2a5867b7b32ba3714a07c
Deepak Hegde <dipakhegde@gmail.com>
2021-05-01 18:35:14 +0530

    Version 3...testing dependency inheritance

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+import {sum} from "rony1256";
+import{GLTFLoader} from "three/examples/jsm/loaders/GLTFLoader";
+
 let a = 66 , b =99;
-console.log(a*b)
+console.log(sum(a,b))
 console.log("ssh test")
+let gltfLoader = new GLTFLoader();
+gltfLoader.load("./port_env.glb", (gltfObject) =>
+{
+
+});
